d351ee1a43178fbb88d051c0142bd94521640684
msnband <55352931+msnband@users.noreply.github.com>
2021-12-03 12:15:56 +0100

    network down
    To get down the network, use it!

--- downNetwork.py
+++ downNetwork.py
@@ -3,10 +3,15 @@ import time
 import subprocess
 
 
-cmd = "/home/moab19/HyperledgerFabric/fabric-samples/test-network"
+cmd = "/home/moab19/HyperledgerFabric_v1/HyperledgerFabric/fabric-samples/test-network"
 os.chdir(cmd)
 os.system("./network.sh down")
 
-time.sleep(3)
+time.sleep(1)
 print()
 print("Network is down...Done!")
+
+
+# netstat -tulnap 
+# sudo kill -9 16967
+
